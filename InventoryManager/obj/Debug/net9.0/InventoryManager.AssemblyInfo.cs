5ee8afdaa86edd8f253bd6e23d3f934952dadfad
zenithcoder0 <zenithcoder0@gmail.com>
2025-06-13 13:04:47 -0500

    system updates

--- InventoryManager/obj/Debug/net9.0/InventoryManager.AssemblyInfo.cs
+++ InventoryManager/obj/Debug/net9.0/InventoryManager.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("InventoryManager")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+30ecec9dbf93276252eafa6b5003d7a2bf7b665e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+35f9c40979df321880453f27275e20741c696d04")]
 [assembly: System.Reflection.AssemblyProductAttribute("InventoryManager")]
 [assembly: System.Reflection.AssemblyTitleAttribute("InventoryManager")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
