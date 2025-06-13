a153aabb93ec724cd50d524180d9f1188da2ac67
zenithcoder0 <zenithcoder0@gmail.com>
2025-06-13 15:21:36 -0500

    cleaned build

--- InventoryManager/Program.cs
+++ InventoryManager/Program.cs
@@ -32,7 +32,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
+// app.UseHttpsRedirection();
 app.UseCors("AllowFrontend");
 app.UseAuthorization();
 app.MapControllers();
--- InventoryManager/obj/Debug/net9.0/InventoryManager.AssemblyInfo.cs
+++ InventoryManager/obj/Debug/net9.0/InventoryManager.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("InventoryManager")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+35f9c40979df321880453f27275e20741c696d04")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5ee8afdaa86edd8f253bd6e23d3f934952dadfad")]
 [assembly: System.Reflection.AssemblyProductAttribute("InventoryManager")]
 [assembly: System.Reflection.AssemblyTitleAttribute("InventoryManager")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
